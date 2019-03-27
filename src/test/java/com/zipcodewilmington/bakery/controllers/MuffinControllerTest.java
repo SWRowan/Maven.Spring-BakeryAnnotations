a919b15c01b11cba1a68813d04a7b5f60a0da7f6
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-03-27 16:45:45 -0400

    All tests passing

--- src/test/java/com/zipcodewilmington/bakery/controllers/MuffinControllerTest.java
+++ src/test/java/com/zipcodewilmington/bakery/controllers/MuffinControllerTest.java
@@ -57,7 +57,8 @@ public class MuffinControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .content(expectedContent)
             )
-            .andExpect(MockMvcResultMatchers.status().isOk())
+           // .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
             .andExpect(MockMvcResultMatchers.content().string(expectedContent));
     }
 }
